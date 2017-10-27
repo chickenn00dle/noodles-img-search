e79fa79bb49cb8d118e47ea8593a9e1b967edd86
Glitch (noodles-img-search) <none>
2017-10-27 13:32:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -7,11 +7,34 @@ const port = process.env.PORT;
 
 
 app.get('/:search', (req, res) => {
-  let search = req.params,
-      query = req.query;
+  const engineID = process.env.SEARCH_ENGINE_ID;
+  const key = process.env.KEY;
+  let search = req.params.search,
+      query = req.query,
+      url = 'https://www.google.com/searchtbm=isch';
+  
+  // console.log(query);
+  
+  search = search.replace(/\s/gi, '+');
+  
+  if (query.offset) {
+    url += '&num=' + query.offset;
+  }
+  
+  url += '&q=' + search;
+  
+  
+  
+  // request(url, (err, res) => {
+  //   if (err) throw err;
+  //   // console.log(res.body);
+  // })
   
 });
 
 app.listen(port);
 
-// https://noodles-img-search.glitch.me/lolcats%20funny?offset=10
+// https://www.google.com/searchtbm=isch
+
+// https://noodles-img-search.glitch.me/cat%20fails?offset=10
+
